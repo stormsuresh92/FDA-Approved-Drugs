9176d8274292752589e416fcee01de470ccf3cf6
Suresh D <38948398+stormsuresh92@users.noreply.github.com>
2025-02-26 17:20:29 +0530

    Update FDA approved drugs.py

--- FDA approved drugs.py
+++ FDA approved drugs.py
@@ -1,14 +1,23 @@
 from requests_html import HTMLSession
 import pandas as pd
-from time import sleep
+from tqdm import tqdm
 
 s = HTMLSession()
 
+
+headers = {
+	'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	'accept-encoding':'gzip, deflate, br, zstd',
+	'accept-language':'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+	'connection':'keep-alive'
+}
+
 def input_url(url):
-	r = s.get(url)
+	r = s.get(url, headers=headers, timeout=10)
 	cont = r.html.find('td > div > div.col-md-12 > ul > li')
 	data = []
-	for drugs in cont:
+	for drugs in tqdm(cont):
 		drug = drugs.text.split('|')[-4].strip()
 		ids = drugs.text.split('|')[-3].split('#')[-1].strip()
 		app = drugs.text.split('|')[-3].split('#')[-2].strip()
@@ -19,7 +28,7 @@ def input_url(url):
 			roaa = 'None'
 		manu = drugs.text.split('|')[-1].strip()
 		data.append([drug, ids, app, roa, roaa, manu])
-		sleep(0.1)
+
 	return data
 
 def save_data(data):
@@ -27,6 +36,7 @@ def save_data(data):
 	df.to_csv('drug dataset.csv', index=False)
 
 def main():
+	#Product Names Beginning with alphabet order
 	url = 'https://www.accessdata.fda.gov/scripts/cder/daf/index.cfm?event=browseByLetter.page&productLetter=Z&ai=0'
 	save_data(input_url(url))
 
